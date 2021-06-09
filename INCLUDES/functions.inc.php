4cabfb6c0b28960aef7c42265f33ce9d2031c113
agentmike007 <manujasan23@gmail.com>
2021-06-09 20:04:07 +0530

    made login module

--- INCLUDES/functions.inc.php
+++ INCLUDES/functions.inc.php
@@ -30,7 +30,7 @@
 
 function usrExists($conn, $email)
 {
-    $sql = "select * from registration where email = ?;";
+    $sql = "select * from users where user_email = ?;";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("location: ../singup.php?error=stmtfailed");
@@ -43,8 +43,8 @@ function usrExists($conn, $email)
     if ($row = mysqli_fetch_assoc($resultData)) {
         mysqli_stmt_close($stmt);
         return $row;
-    } else {
-        $result = false;
+    } 
+    else {
         mysqli_stmt_close($stmt);
         return false;
     }
@@ -121,26 +121,26 @@ function emptyInputLogin($user, $pwd)
     return $result;
 }
 
-function loginUser($conn, $user, $pwd)
+function loginUser($conn, $email, $pwd)
 {
-    $uidExists = uidExists($conn, $user, $user);
+    $usrExists = usrExists($conn, $email);
 
-    if ($uidExists === false) {
+    if ($usrExists === false) {
         header("location: ../login.php?error=wronglogin");
         exit();
     }
 
-    $pwdHashed = $uidExists["usersPwd"];
+    $pwdHashed = $usrExists["user_pwd"];
     $checkPwd = password_verify($pwd, $pwdHashed);
 
     if ($checkPwd === false) {
         header("location: ../login.php?error=wrongpass");
         exit();
-    } else if ($checkPwd === true) {
+    } 
+    else if ($checkPwd === true) {
         session_start();
-        $_SESSION["userid"] = $uidExists["usersID"];
-        $_SESSION["useruid"] = $uidExists["usersUid"];
-        header("location: ../index.php");
+        $_SESSION["user_email"] = $usrExists["user_email"];
+        header("location: ../dashboard.php");
         exit();
     }
 }
