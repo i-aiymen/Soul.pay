e4edbc19f975ca421a93de44296efbdac8dcd97d
MUHAMMED AYIMEN <aiymenlatheef03@gmail.com>
2021-06-13 11:40:09 +0530

    User dashboard updated

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+let sidebar = document.querySelector(".l-navbar");
+let sidebarBtn = document.querySelector(".sidebarBtn");
+
+sidebarBtn.onclick = function() {
+    sidebar.classList.toggle("show");
+    sidebarBtn.classList.toggle("rotate");
+}
+
+const linkColor = document.querySelectorAll('.nav__link');   
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'));
+    this.classList.add('active');
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink));
+
+
+
+
+const linkCollapse = document.getElementsByClassName('collapse__link')
+var i
+
+for(i=0;i<linkCollapse.length;i++){
+  linkCollapse[i].addEventListener('click', function(){
+    const collapseMenu = this.nextElementSibling
+    collapseMenu.classList.toggle('showCollapse')
+
+    const rotate = collapseMenu.previousElementSibling
+    rotate.classList.toggle('rotate')
+  })
+}
+
